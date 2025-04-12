e91743c17dcb33307d4af386751595343129ff08
tawfiqueYeasin <g.towfiq@yahoo.com>
2025-04-12 02:28:03 -0400

    edited add method, added new add method

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -3,7 +3,13 @@ package com.git.learning;
 public class Calculator {
 
 	public int add(int i, int j) {
-		return i + j;
+		int result = 0 ;
+		result = i + j;
+		return result;
+	}
 
+	public int multiply(int i, int j) {
+		return i * j;
 	}
 }
+ 
